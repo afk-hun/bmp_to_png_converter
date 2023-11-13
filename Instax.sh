e6f4b5bf1b3c0fcea2ee270d66b2b247d148c01b
Ákos Ferenc Kalamár <akos@akoss-air.home>
2023-11-13 16:33:26 +0100

    my own root path fix in sh files

--- Instax.sh
+++ Instax.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env node
 
 cameraType="Instax"
-rootDir="/Users/akos/Development/gitRepos/bmp_to_png/"
+rootDir="/Users/akos/Development/gitRepos/bmp_to_png_converter/"
 index="${rootDir}index.js"
 
 node $index $cameraType $rootDir
--- Polaroid.sh
+++ Polaroid.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env node
 
 cameraType="Polaroid"
-rootDir="/Users/akos/Development/gitRepos/bmp_to_png/"
+rootDir="/Users/akos/Development/gitRepos/bmp_to_png_converter/" #TODO: this path is your own root
 index="${rootDir}index.js"
 
 node $index $cameraType $rootDir
